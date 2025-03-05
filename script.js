690c64436564a2e51dc5be33b284c289f57c24a2
lev-voronovych <vlev0760@gmail.com>
2025-03-05 21:56:02 +0200

    add js

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let age = 13;
+console.log(age);
+
+let name = "Лев";
+console.log(name);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "Життя – це те, що з тобою відбувається, поки ти будуєш плани.";
+console.log(myString);
+
+let myNumber = 5;
+myNumber += 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+
+let userName = prompt("Введіть своє ім’я");
+alert("Ваше ім'я: " + userName);
+console.log(typeof userName);
+
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+alert("Увага! Дія може бути небезпечною.");
+let isConfirmed = confirm("Ви впевнені, що хочете продовжити?");
+console.log(isConfirmed);
